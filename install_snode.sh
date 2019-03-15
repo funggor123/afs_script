66163cff40419128f13fd1b4b03840d6c8cc2c24
Cheng Chi Fung <chifcheng9@Chengde-MacBook-Pro.local>
2019-03-15 15:26:15 +0800

    hi

--- install_snode.sh
+++ install_snode.sh
@@ -89,7 +89,7 @@ if [ "$is_go_installed" = true ] ; then
        else
             echo "Mongodb is not detected, is going to install now" 
             apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
-            echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+            echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
             apt-get update
             apt-get install -y mongodb-org
             service mongod start
