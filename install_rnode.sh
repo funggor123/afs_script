7c527f562107165b0aa7b62c94533547b5bbecb2
Cheng Chi Fung <chifcheng9@Chengde-MacBook-Pro.local>
2019-03-15 15:09:23 +0800

    hi

--- install_rnode.sh
+++ install_rnode.sh
@@ -16,10 +16,10 @@ if [ "$is_go_installed" = false ] ; then
    if [ -z "$goroot" ] ; then
         goroot="/home/steven/go"
    fi
-   echo "Please input the go path (Default: /home/steven/work/go/): "
+   echo "Please input the go path (Default: /home/steven/work/): "
    read gopath
    if [ -z "$gopath" ] ; then
-        gopath="/home/steven/work/go"
+        gopath="/home/steven/work"
    fi
    cd /tmp
    if [ "$OSTYPE" = "linux-gnu" ] ; then
@@ -51,7 +51,7 @@ if [ "$is_go_installed" = false ] ; then
 fi
 
 if [ "$is_go_installed" = true ] ; then
-       goro=$(go env GOPATH)
+       goro=$(go env GOPATH)/src
        echo $goro       
        echo "--------------(2)--Install Git---------------" 
        if which git >/dev/null; then
